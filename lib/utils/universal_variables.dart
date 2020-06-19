7b42c9c9098358d39db5eb58b47e8dd3648600a9
Taki Uddin <takiuddin93@gmail.com>
2020-06-20 02:32:11 +0600

    files in utils folder updated

--- lib/utils/universal_variables.dart
+++ lib/utils/universal_variables.dart
@@ -7,7 +7,6 @@ class UniversalVariables {
   static final Color primaryAlabaster = Color(0xFFFAFAFA);
   static final Color primaryRiverBed = Color(0xFF424C55);
   static final Color primaryEbony = Color(0xFF04080F);
-  static final Color primaryEbonyShadow = Color(0x2904080F);
   static final Color primaryGhost = Color(0xFFCACFD6);
   static final Color primaryGhostShadow = Color(0x29CACFD6);
   static final Color primaryCrimson = Color(0xFFDA2323);
