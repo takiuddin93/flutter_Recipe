bd247f21955f31c7a5bf6dd31e036b1cb8ebb8e2
Taki Uddin <takiuddin93@gmail.com>
2020-06-20 02:27:22 +0600

    constants folder updated

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,5 +1,3 @@
 const String LOGIN_API_URL = "https://rcapp.utech.dev/api/auth/login";
 const String USER_DETAILS_API_URL = "https://rcapp.utech.dev/api/user";
 const String RECIPE_DETAILS_API_URL = "https://rcapp.utech.dev/api/recipes";
-
-const String TEST = "https://corona-api.cramstack.com/api/v1/districts";
